245fac58c15a8129e107b4a61425598a5b0b6410
Camilo Lopes <camilo@camilolopes.com.br>
2013-06-30 22:35:37 -0300

    first commit I4JSF

---
+++ I4JSF/src/main/java/com/i4jsf/interfaces/IJsf.java
@@ -0,0 +1,8 @@
+package com.i4jsf.interfaces;
+
+public interface IJsf {
+
+	void addTranslateContext(String bundleVar, String keyProperties);
+	void addTranslateContext(String componentId,String bundleVar, String keyProperties);
+
+}
