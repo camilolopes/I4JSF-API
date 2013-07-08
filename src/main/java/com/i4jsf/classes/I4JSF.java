719aff86c55428ddbd0504d7352c2d42272a6e91
Camilo Lopes <camilo@camilolopes.com.br>
2013-07-07 23:55:09 -0300

    added java-doc comment

--- src/main/java/com/i4jsf/classes/I4JSF.java
+++ src/main/java/com/i4jsf/classes/I4JSF.java
@@ -12,10 +12,7 @@ import com.i4jsf.interfaces.IJsf;
 public class I4JSF implements IJsf{
 	
 	
-	/**
-	 * @param bundleVar
-	 * @param keyProperties
-	 */
+
 	public void addTranslateContext(String bundleVar, String keyProperties){
 		FacesContext context = FacesContext.getCurrentInstance();
 		ResourceBundle bundle = context.getApplication().getResourceBundle(context, bundleVar);
@@ -24,55 +21,31 @@ public class I4JSF implements IJsf{
 	}
 	
 	
-/**
- * @param componentId JSF
- * @param bundleVar internationalization variable 
- * @param keyProperties internationalization 
- */
-	public void addTranslateContext(String componentId, String bundleVar,String keyProperties) {
+public void addTranslateContext(String componentId, String bundleVar,String keyProperties) {
 		FacesContext context = FacesContext.getCurrentInstance();
 		ResourceBundle bundle = context.getApplication().getResourceBundle(context, bundleVar);
 		String message = bundle.getString(keyProperties);
 		addFacesMessages(componentId, context, message);
 	}
 
-
-	/**
-	 * 
-	 * @param severity INFO, WARN, ERROR 
-	 * @param msg message 
-	 */
 	public void  addMessageFaceContext(String idComponent,Severity severity, String msg) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		FacesMessage facesMessage = new FacesMessage(severity, msg ,"");
 		facesContext.addMessage(idComponent, facesMessage);
 	}
 
-	/**
-	 * 
-	 * @param context
-	 * @param message
-	 */
+
 	public void addFacesMessages(FacesContext context, String message) {
 		FacesMessage faceMessages = new FacesMessage(message);
 		context.addMessage(null, faceMessages);
 	}
-	/**
-	 * add message to context for specific component
-	 * @param componentId
-	 * @param context
-	 * @param message
-	 */
+
 	public void addFacesMessages(String componentId, FacesContext context,	String message) {
 		FacesMessage facesMessage = new FacesMessage(message);
 		context.addMessage(componentId, facesMessage);
 	}
 
-	/**
-	 * 
-	 * @param severity INFO, WARN, ERROR 
-	 * @param msg
-	 */
+
 	public void addMessageFaceContext(Severity severity, String msg) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		FacesMessage facesMessage = new FacesMessage(severity, msg ,"");
--- src/main/java/com/i4jsf/interfaces/IJsf.java
+++ src/main/java/com/i4jsf/interfaces/IJsf.java
@@ -4,19 +4,46 @@ import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
 
 public interface IJsf {
-
+	/**
+	 *add message non specific component id. Message coming from bundle properties
+	 * @param bundleVar
+	 * @param keyProperties
+	 */
 	public void addTranslateContext(String bundleVar, String keyProperties);
-
+	/**
+	 * add specific message coming from bundle to specific component id
+	 * @param componentId
+	 * @param bundleVar internationalization variable 
+	 * @param keyProperties internationalization 
+	 */
+		
 	public void addTranslateContext(String componentId, String bundleVar,
 			String keyProperties);
-
-	public void addMessageFaceContext(String idComponent, Severity severity,
+	/**
+	 * add message to specific component via id, informing severity and message
+	 * @param severity INFO, WARN, ERROR 
+	 * @param msg message 
+	 */
+	public void addMessageFaceContext(String componentId, Severity severity,
 			String message);
-
+	/**
+	 * add message to context
+	 * @param context
+	 * @param message
+	 */
 	public void addFacesMessages(FacesContext context, String message);
-
+	/**
+	 * add message to context for specific component by id 
+	 * @param componentId
+	 * @param context
+	 * @param message
+	 */
 	public void addFacesMessages(String componentId, FacesContext context,
 			String message);
-
+	/**
+	 * add message to context with specific Severity 
+	 * @param severity INFO, WARN, ERROR 
+	 * @param msg
+	 */
 	public void addMessageFaceContext(Severity severity, String message);
 }
